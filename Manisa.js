3273bdf0fe74e3bf008bb28670ca1c476f44e9e0
RaphaDevsearch <mizaraphaelpro@gmail.com>
2025-08-20 22:32:34 +0530

    prototype isa lasa soratra

---
+++ Manisa.js
@@ -0,0 +1,20 @@
+class Manisa{
+    constructor(){
+        this.isa = 0;
+        this.isaSoratra = "isa lasa soratra";
+    }
+
+    manisa(isa){
+        this.isa = isa;
+        return this.isaSoratra;
+    }
+
+    isaFeo(){
+        console.log(`feo : ${this.isaSoratra}`);
+    }
+
+    dikateny(){
+        console.log(`dikateny hafa ${this.isaSoratra}`);
+    }
+
+}
